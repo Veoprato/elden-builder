6d6f04f83219fc1b6a174a20ca9d0a9fa6f839a3
Veoprato <75872616+Veoprato@users.noreply.github.com>
2022-04-14 00:12:27 -0400

    initial front-end files with React

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
